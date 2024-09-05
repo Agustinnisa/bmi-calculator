f0fbe98db5c28159babec1883a11e496204d3475
Agustinnisa <nisaagustin6@gmail.com>
2024-09-05 16:54:06 +0700

    add js function

---
+++ index.js
@@ -0,0 +1,25 @@
+document.getElementById('bmi-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Ambil nilai input
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+    // Calculate BMI
+    let bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+
+    // Status BMI
+    let status = '';
+    if (bmi < 18.5) {
+        status = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        status = 'Normal Weight'
+    } else if (bmi >= 25 && 29.9) {
+        status = 'Overweight';
+    } else {
+        status = 'Obesity'
+    }
+
+    document.getElementById('bmi-result').textContent = bmi;
+    document.getElementById('bmi-status').textContent = status;
+});
